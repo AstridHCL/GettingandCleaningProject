8918e30c10bb27f56bc8cf6617b49bc4f5d174a3
AstridHCL <asthcl@gmail.com>
2016-03-29 23:18:30 -0430

    Added files via upload

---
+++ run_analysis.R
@@ -0,0 +1,57 @@
+## Getting and Cleaning Data Course Project
+## Astrid Correa
+
+# Labes Lecture
+activity_labels <- read.table("activity_labels.txt",stringsAsFactors = FALSE)
+features <- read.table("features.txt")
+# Test Folder
+subject_test <- read.table("./test/subject_test.txt")
+x_test <- read.table("./test/X_test.txt")
+y_test <- read.table("./test/y_test.txt")
+body_acc_x_test <- read.table("./test/Inertial Signals/body_acc_x_test.txt")
+body_acc_y_test <- read.table("./test/Inertial Signals/body_acc_y_test.txt")
+body_acc_z_test <- read.table("./test/Inertial Signals/body_acc_z_test.txt")
+body_gyro_x_test <- read.table("./test/Inertial Signals/body_gyro_x_test.txt")
+body_gyro_y_test <- read.table("./test/Inertial Signals/body_gyro_y_test.txt")
+body_gyro_z_test <- read.table("./test/Inertial Signals/body_gyro_z_test.txt")
+total_acc_x_test <- read.table("./test/Inertial Signals/total_acc_x_test.txt")
+total_acc_y_test <- read.table("./test/Inertial Signals/total_acc_y_test.txt")
+total_acc_z_test <- read.table("./test/Inertial Signals/total_acc_z_test.txt")
+# Train Folder
+subject_train <- read.table("./train/subject_train.txt")
+x_train <- read.table("./train/X_train.txt")
+y_train <- read.table("./train/y_train.txt")
+body_acc_x_train <- read.table("./train/Inertial Signals/body_acc_x_train.txt")
+body_acc_y_train <- read.table("./train/Inertial Signals/body_acc_y_train.txt")
+body_acc_z_train <- read.table("./train/Inertial Signals/body_acc_z_train.txt")
+body_gyro_x_train <- read.table("./train/Inertial Signals/body_gyro_x_train.txt")
+body_gyro_y_train <- read.table("./train/Inertial Signals/body_gyro_y_train.txt")
+body_gyro_z_train <- read.table("./train/Inertial Signals/body_gyro_z_train.txt")
+total_acc_x_train <- read.table("./train/Inertial Signals/total_acc_x_train.txt")
+total_acc_y_train <- read.table("./train/Inertial Signals/total_acc_y_train.txt")
+total_acc_z_train <- read.table("./train/Inertial Signals/total_acc_z_train.txt")
+
+## Label the data
+names(x_test)<- features$V2
+names(x_train)<-features$V2
+names(subject_test)<- "Subject"
+names(subject_train)<- "Subject"
+y_test_label <- factor(y_test$V1, labels = activity_labels$V2)
+y_train_label <- factor(y_train$V1, labels = activity_labels$V2)
+## Extracting the data
+colum <- grep("mean()|std()",features$V2, value = TRUE)
+test_selec <- data.frame(Subject= subject_test,Activity =y_test_label,x_test[,colum])
+train_selec <- data.frame(Subject= subject_train,Activity= y_train_label,x_train[,colum])
+## Merge the data
+x_set <- merge(test_selec,train_selec, all = TRUE)
+
+## Tidy independents data
+
+
+# Average of each variable for each activity and each subject
+SplitActivity <- split(x_set,x_set$Activity)
+val <- c(1,3:length(SplitActivity$WALKING))
+CleanActivity <- lapply(SplitActivity,function(x) x[,val] ) #Extract the Activity
+ForActivityForSubject <- lapply(CleanActivity, function(y) apply(y,2, function(x) tapply(x,y[,1],mean))) 
+
+write.table(ForActivityForSubject,"Step5.txt",row.names = FALSE)
